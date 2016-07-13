466827e23071f4676e0b629dccc0dd299a761c36
eevans01 <eevans01@ferrisisd.org>
2016-07-12 20:01:42 -0500

    Create BPJLesson01Lesson.java

---
+++ java/blue-pelican-java/lesson01/BPJLesson01Lesson.java
@@ -0,0 +1,16 @@
+//Program Name: Blue Pelican Java Lesson 01 (Lesson)
+//Programmer Name: Eric Evans, M.Ed.
+//Programmer Organization: Ferris High School
+//Program Date: Fall 2016
+
+public class BPJLesson01Lesson {
+    public static void main(String args[]){
+        System.out.println("Hello World"); // Outputs the literal string of text - Hello World.
+        System.out.println("Peter Piper picked a peck of pickeled peppers."); // Outputs the literal string of text - Peter Piper picked a peck of pickeled peppers.
+        System.out.println("I like computer science."); // Outputs the literal string of text - I like computer science.
+        System.out.println(25/5); // Outputs the answer to the division problem 25/5.
+        System.out.println(25/4); // Outputs the answer to the division problem 25/4.
+        System.out.println(4/7.0445902); // Outputs the answer to the division problem 4/7.0455902.
+        System.out.println(13*159.56); // Outputs the answer to the multiplication problem 13*159.56.
+    }
+}
