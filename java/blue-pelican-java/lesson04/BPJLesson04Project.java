e52c15fb8c4fb1c82192f5677a92877a08648ffa
eevans01 <eevans01@ferrisisd.org>
2016-07-12 20:10:55 -0500

    Create BPJLesson04Project.java

---
+++ java/blue-pelican-java/lesson04/BPJLesson04Project.java
@@ -0,0 +1,37 @@
+//Program Name: Blue Pelican Java Lesson 04 (Project)
+//Programmer Name: Eric Evans, M.Ed.
+//Programmer Organization: Ferris High School
+//Program Date: Fall 2016
+
+public class BPJLesson04Project {
+    public static void main(String args[]){
+        // ***** SETUP PROBLEM #1 TO BE SOLVED *****
+            // Step 1. prob1 = 79 + 3 * (4 + 82 - 68) - 7 + 19
+            // Step 2. prob1 = 79 + 3 * 18 - 7 + 19
+            // Step 3. prob1 = 79 + 54 - 7 + 19
+            // Step 4. prob1 = 133 - 7 + 19
+            // Step 5. prob1 = 126 + 19
+            // Step 6. prob1 = 145
+        String prob1 = "79 + 3 * (4 + 82 - 68) - 7 + 19";
+        int prob1Answer = 79 + 3 * (4 + 82 - 68) - 7 + 19;
+        System.out.println(prob1 + " = " + prob1Answer);
+        System.out.println("");
+        // ***** SETUP PROBLEM #2 TO BE SOLVED *****
+            // Step 1. prob2 = (179 + 21 + 10) / 7 + 181
+            // Step 2. prob2 = 210 / 7 + 181
+            // Step 3. prob2 = 30 + 181
+            // Step 4. prob2 = 211
+        String prob2 = "(179 + 21 + 10) / 7 + 181";
+        int prob2Answer = (179 + 21 + 10) / 7 + 181;
+        System.out.println(prob2 + " = " + prob2Answer);
+        System.out.println("");
+        // ***** SETUP PROBLEM #3 TO BE SOLVED *****
+            // Step 1. prob3 = 10389 * 56 * 11 + 2246
+            // Step 2. prob3 = 581784 * 11 + 2246
+            // Step 3. prob3 = 6399624 + 2246
+            // Step 4. prob3 = 6401870
+        String prob3 = "10389 * 56 * 11 + 2246";
+        int prob3Answer = 10389 * 56 * 11 + 2246;
+        System.out.println(prob3 + " = " + prob3Answer);
+    }
+}
