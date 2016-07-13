3864b7775341c40646edd2925fe95576a000159d
eevans01 <eevans01@ferrisisd.org>
2016-07-12 20:02:21 -0500

    Create BPJLesson01Project.java

---
+++ java/blue-pelican-java/lesson01/BPJLesson01Project.java
@@ -0,0 +1,16 @@
+//Program Name: Blue Pelican Java Lesson 01 (Project)
+//Programmer Name: Eric Evans, M.Ed.
+//Programmer Organization: Ferris High School
+//Program Date: Fall 2016
+
+public class BPJLesson01Project {
+    public static void main(String args[]){
+        System.out.println("From: Bill Smith");
+        System.out.println("Address: Dell Computer, Bldg 13");
+        System.out.println("Date: April 12, 2005");
+        System.out.println("");
+        System.out.println("To: Jack Jones");
+        System.out.println("");
+        System.out.println("Message: Help! I'm trapped inside a computer!");
+    }
+}
