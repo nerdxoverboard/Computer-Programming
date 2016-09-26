e8382bdcd755ea0873179afa9164e3294f11b50a
eevans01 <eevans01@ferrisisd.org>
2016-09-26 17:13:18 -0500

    Create step1.java

---
+++ calculator/step1.java
@@ -0,0 +1,14 @@
+//Program Name: 4-Function Calculator
+//Programmer Name: Your Name
+//Programmer Organization: Ferris High School
+//Program Date: Fall 2016
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class calculator{
+    public static void main (String[] args) throws java.lang.Exception{
+        
+    }
+}
