7a33716464e1342e7aa6d4432b18970365f20e61
eevans01 <eevans01@ferrisisd.org>
2016-09-26 17:18:31 -0500

    Create step2.java

---
+++ calculator/step2.java
@@ -0,0 +1,22 @@
+//Program Name: 4-Function Calculator
+//Programmer Name: Your Name
+//Programmer Organization: Ferris High School
+//Program Date: Fall 2016
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class calculator{
+    public static void main (String[] args) throws java.lang.Exception{
+        Scanner menuChoice = new Scanner(System.in);
+        System.out.println("Basic Calculator");
+        System.out.println("1 - Addition");
+        System.out.println("2 - Subtraction");
+        System.out.println("3 - Multiplication");
+        System.out.println("4 - Division");
+        System.out.println("");
+        System.out.print("Enter Your Selection_");
+        int menu = menuChoice.nextInt();
+    }
+}
